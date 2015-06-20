1d152b2f6081a0a12c945268e7f0bd227480bf84
Erica <erica.anne.low@gmail.com>
2015-06-20 13:56:28 -0700

    got rid of global variables - quiz object holds state

--- quiz.js
+++ quiz.js
@@ -1,5 +1,76 @@
-var currentIndex = 0;
-var userAnswers = [];
+var quiz = {
+	currentIndex: 0,
+	userAnswers: [],
+	
+	displayQuestion: function(index) {
+		//question
+		var questionDiv = document.getElementById("question");
+		questionDiv.innerHTML = ""; //erase contents
+		var questionh1 = document.createElement("h1");
+		questionDiv.appendChild(questionh1);
+		questionh1.innerHTML = allQuestions[index].question;
+		questionh1.setAttribute("id", "questionh1");
+		
+		//answers
+		var answersDiv = document.getElementById("answers");
+		answersDiv.innerHTML = ""; //erase contents
+		for (var i=0; i<allQuestions[index].choices.length; i++) {
+			
+			var p = document.createElement("p");
+			answersDiv.appendChild(p);
+			
+			var input = document.createElement("input");
+			input.setAttribute("type", "radio");
+			input.setAttribute("name", "answers");
+			var inputID = "answer" + i;
+			var inputValue = i;
+			input.setAttribute("id", inputID);
+			input.setAttribute("value", inputValue);
+			p.appendChild(input);
+			
+			var label = document.createElement("label");
+			label.setAttribute("for", inputID);
+			label.innerHTML = allQuestions[index].choices[i];
+			p.appendChild(label);
+		}
+	},
+	
+	displayFinish: function() {
+		var correctAnswers = 0;
+		var totalAnswers = quiz.userAnswers.length
+		
+		//get all the answers and check them
+		for (var i=0; i < totalAnswers; i++) {
+			if (quiz.userAnswers[i] == allQuestions[i].correctAnswer) {
+				correctAnswers++;
+			}
+		}
+		//remove everything and display result
+		var wrapper = document.getElementById("wrapper");
+		wrapper.innerHTML = "";
+		var h1 = document.createElement("h1");
+		wrapper.appendChild(h1);
+		h1.innerHTML = "You got " + correctAnswers + " out of " + totalAnswers + " questions correct.";
+		
+		var p = document.createElement("p");
+		wrapper.appendChild(p);
+		
+		if (correctAnswers <= (totalAnswers/2)) {
+			p.innerHTML = "That's pretty bad. <br>"
+			var img = document.createElement("img");
+			p.appendChild(img);
+			img.setAttribute("src", "timlose.jpg");
+			
+		} else {
+			p.innerHTML = "That's pretty good. <br>"
+			var img = document.createElement("img");
+			p.appendChild(img);
+			img.setAttribute("src", "timwin.gif");
+		}
+		
+	}
+	
+}
 
 var allQuestions = [
 	{
@@ -24,91 +95,22 @@ var allQuestions = [
 	}
 ];
 
-function displayQuestion(index) {
-	
-	//question
-	var questionDiv = document.getElementById("question");
-	questionDiv.innerHTML = ""; //erase contents
-	var questionh1 = document.createElement("h1");
-	questionDiv.appendChild(questionh1);
-	questionh1.innerHTML = allQuestions[index].question;
-	questionh1.setAttribute("id", "questionh1");
-	
-	//answers
-	var answersDiv = document.getElementById("answers");
-	answersDiv.innerHTML = ""; //erase contents
-	for (var i=0; i<allQuestions[index].choices.length; i++) {
-		
-		var p = document.createElement("p");
-		answersDiv.appendChild(p);
-		
-		var input = document.createElement("input");
-		input.setAttribute("type", "radio");
-		input.setAttribute("name", "answers");
-		var inputID = "answer" + i;
-		var inputValue = i;
-		input.setAttribute("id", inputID);
-		input.setAttribute("value", inputValue);
-		p.appendChild(input);
-		
-		var label = document.createElement("label");
-		label.setAttribute("for", inputID);
-		label.innerHTML = allQuestions[index].choices[i];
-		p.appendChild(label);
-	}
-	
-};
 
 
 window.onload = function() {
-	displayQuestion(0);
+	quiz.displayQuestion(0);
 	var nextButton = document.getElementById("nextButton");
 	nextButton.onclick = function() {
 		//get answer
-		userAnswers[currentIndex] = document.querySelector('input[name="answers"]:checked').value;
+		quiz.userAnswers[quiz.currentIndex] = document.querySelector('input[name="answers"]:checked').value;
 		
 		//display next question
-		if (currentIndex < allQuestions.length - 1) {
-			currentIndex++;
-			displayQuestion(currentIndex);
+		if (quiz.currentIndex < allQuestions.length - 1) {
+			quiz.currentIndex++;
+			quiz.displayQuestion(quiz.currentIndex);
 		} else {
-			displayFinish();
+			quiz.displayFinish();
 		}
 	};
 }
-
-function displayFinish() {
-	var correctAnswers = 0;
-	var totalAnswers = userAnswers.length
-	
-	//get all the answers and check them
-	for (var i=0; i < totalAnswers; i++) {
-		if (userAnswers[i] == allQuestions[i].correctAnswer) {
-			correctAnswers++;
-		}
-	}
-	
-	//remove everything and display result
-	var wrapper = document.getElementById("wrapper");
-	wrapper.innerHTML = "";
-	var h1 = document.createElement("h1");
-	wrapper.appendChild(h1);
-	h1.innerHTML = "You got " + correctAnswers + " out of " + totalAnswers + " questions correct.";
-	
-	var p = document.createElement("p");
-	wrapper.appendChild(p);
-	
-	if (correctAnswers <= (totalAnswers/2)) {
-		p.innerHTML = "That's pretty bad. <br>"
-		var img = document.createElement("img");
-		p.appendChild(img);
-		img.setAttribute("src", "timlose.jpg");
-		
-	} else {
-		p.innerHTML = "That's pretty good. <br>"
-		var img = document.createElement("img");
-		p.appendChild(img);
-		img.setAttribute("src", "timwin.gif");
-	}
-	
-}
+ 
