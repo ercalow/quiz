54349a58173f1a217fd5478c1099c7363d0dad14
Erica <erica.anne.low@gmail.com>
2015-06-24 19:52:37 -0700

    jquery slides. changed .text to .html. need to rethink structure.

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,17 +1,17 @@
 var quiz = {
 	currentIndex: 0,
 	userAnswers: [],
-	
+
 	displayQuestion: function(index) {
-		//question
-		var questionDiv = $("#question").text(""); //erase contents
+		//create question
+		var questionDiv = $("#question");
 		var questionh1 = $("<h1/>").attr({
 			id: "questionh1"
-			}).text(timanderic[index].question);
+			}).html(timanderic[index].question);
 		questionDiv.append(questionh1);
 		
-		//answers
-		var answersDiv = $("#answers").text("");
+		//create answers
+		var answersDiv = $("#answers");
 		for (var i = 0; i < timanderic[index].choices.length; i++) {
 			
 			var p = $("<p/>");
@@ -27,10 +27,13 @@ var quiz = {
 			
 			var label = $("<label/>").attr({
 				"for": "answer" + i
-			}).text(timanderic[index].choices[i]);
+			}).html(timanderic[index].choices[i]);
 			
 			p.append(label);
 		}
+		
+		//display all
+		$("#wrapper").slideDown("fast");
 	},
 	
 	displayFinish: function() {
@@ -43,9 +46,11 @@ var quiz = {
 				correctAnswers++;
 			}
 		}
-		//remove everything and display result
-		var wrapper = $("#wrapper").text("");
-		var h1 = $("<h1/>").text("You got " + correctAnswers + " out of " + totalAnswers + " questions correct.");
+		//erase contents
+		var wrapper = $("#wrapper").html("");
+		
+		//score
+		var h1 = $("<h1/>").html("You got " + correctAnswers + " out of " + totalAnswers + " questions correct.");
 		wrapper.append(h1);
 		
 		var p = $("<p/>");
@@ -53,7 +58,7 @@ var quiz = {
 		
 		//if you get 50% or less
 		if (correctAnswers <= (totalAnswers/2)) {
-			p.text("That's pretty bad.");
+			p.html("That's pretty bad.");
 			var img = $("<img/>").attr({
 				src: "images/timlose.jpg"
 			});
@@ -61,30 +66,44 @@ var quiz = {
 		
 		//if you get over 50%
 		} else {
-			p.text("That's pretty good.");
+			p.html("That's pretty good.");
 			var img = $("<img/>").attr({
 				src: "images/timwin.gif"
 			});
 			wrapper.append(img);	
 		}
 		
+		//display all
+		//display all
+		$("#wrapper").slideDown("fast");
 	}
-	
 }
 
 
 window.onload = function() {
-	quiz.displayQuestion(quiz.currentIndex);
+	
+	$("#startButton").on("click", function() {
+		$(this).css("display", "none");
+		quiz.displayQuestion(quiz.currentIndex);
+	});
+	
+	
 	$("#nextButton").on("click", function() {
 		//store answer
 		quiz.userAnswers[quiz.currentIndex] = $('input[name="answers"]:checked').val();
+		//slide up and erase
+		$("#wrapper").slideUp("fast", function() {
+				$("#question").html("");
+				$("#answers").html("")
+				if (quiz.currentIndex < timanderic.length - 1) {
+					quiz.currentIndex++;
+					quiz.displayQuestion(quiz.currentIndex);
+				} else {
+					quiz.displayFinish();
+				}
+			});
 		//display next question
-		if (quiz.currentIndex < timanderic.length - 1) {
-			quiz.currentIndex++;
-			quiz.displayQuestion(quiz.currentIndex);
-		} else {
-			quiz.displayFinish();
-		}
+		
 		
 	});
 }
