911270ba50ca1a0d26b5aac9fba4c276515137ff
Erica <erica.anne.low@gmail.com>
2015-06-24 23:23:26 -0700

    restart. now displayQuestion completely recreates elements.

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,17 +1,52 @@
+/*
+TO DO
+	choose which quiz you want
+	restart button
+		wipe all quiz properties
+	do different animations. can i randomize?
+		
+	test on IE9
+		
+	get rid of radio buttons
+	center
+	make it look good 
+	rwd 
+		text too small on phone
+		do i need jquery mobile?
+	
+*/
+
 var quiz = {
 	currentIndex: 0,
 	userAnswers: [],
+	
 
 	displayQuestion: function(index) {
+		//get wrapper
+		var wrapper = $("#wrapper");
+		
+		//image
+		var img = $("<img/>").attr({
+				src: "images/tim.jpg"
+			});
+		wrapper.append(img);
+		
 		//create question
-		var questionDiv = $("#question");
+		var questionDiv = $("<div/>").attr({
+			id: "question"
+		});
 		var questionh1 = $("<h1/>").attr({
 			id: "questionh1"
-			}).html(timanderic[index].question);
+		}).html(timanderic[index].question);
+		wrapper.append(questionDiv);
 		questionDiv.append(questionh1);
 		
 		//create answers
-		var answersDiv = $("#answers");
+		var answersDiv = $("<div/>").attr({
+			id: "answers"
+		});
+		wrapper.append(answersDiv);
+		
 		for (var i = 0; i < timanderic[index].choices.length; i++) {
 			
 			var p = $("<p/>");
@@ -32,17 +67,38 @@ var quiz = {
 			p.append(label);
 		}
 		
+		var nextButton = $("<button/>").attr({
+			id: "nextButton",
+		}).html("NEXT");
+		wrapper.append(nextButton);
+		
+		//event listener for next button
+		$("#nextButton").on("click", function() {
+			//store answer
+			quiz.userAnswers[quiz.currentIndex] = $('input[name="answers"]:checked').val();
+			//slide up and erase
+			$("#wrapper").fadeOut("fast", function() {
+					wrapper.html("");
+					if (quiz.currentIndex < timanderic.length - 1) {
+						quiz.currentIndex++;
+						quiz.displayQuestion(quiz.currentIndex);
+					} else {
+						quiz.displayFinish();
+					}
+				});
+		});
+		
 		//display all
-		$("#wrapper").slideDown("fast");
+		wrapper.fadeIn("fast");
 	},
 	
 	displayFinish: function() {
 		var correctAnswers = 0;
-		var totalAnswers = quiz.userAnswers.length
+		var totalAnswers = this.userAnswers.length 
 		
 		//get all the answers and check them
 		for (var i = 0; i < totalAnswers; i++) {
-			if (quiz.userAnswers[i] == timanderic[i].correctAnswer) {
+			if (this.userAnswers[i] == timanderic[i].correctAnswer) {
 				correctAnswers++;
 			}
 		}
@@ -73,38 +129,48 @@ var quiz = {
 			wrapper.append(img);	
 		}
 		
+		//restart button
+		var restartButton = $("<button/>").attr({
+			id: "restartButton",
+		}).html("RESTART");
+
+		wrapper.append(restartButton);
+		restartButton.on("click", quiz.restart);
+		
 		//display all
 		//display all
-		$("#wrapper").slideDown("fast");
+		$("#wrapper").fadeIn("fast");
+	},
+	
+	restart: function() {
+		//reset score
+		quiz.currentIndex = 0;
+		quiz.userAnswers = [];
+		
+		//empty 
+		
+		var wrapper = $("#wrapper");
+		wrapper.fadeOut("slow", function() {
+			wrapper.html("");
+			quiz.displayQuestion(quiz.currentIndex);
+		});
+		
+		
+		
 	}
-}
+};
 
 
 window.onload = function() {
 	
+	//set start event listener
 	$("#startButton").on("click", function() {
 		$(this).css("display", "none");
 		quiz.displayQuestion(quiz.currentIndex);
 	});
 	
 	
-	$("#nextButton").on("click", function() {
-		//store answer
-		quiz.userAnswers[quiz.currentIndex] = $('input[name="answers"]:checked').val();
-		//slide up and erase
-		$("#wrapper").slideUp("fast", function() {
-				$("#question").html("");
-				$("#answers").html("")
-				if (quiz.currentIndex < timanderic.length - 1) {
-					quiz.currentIndex++;
-					quiz.displayQuestion(quiz.currentIndex);
-				} else {
-					quiz.displayFinish();
-				}
-			});
-		//display next question
-		
-		
-	});
 }
+
+
  
